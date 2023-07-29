c700475a3a26d90904c54e7a808bdf19d63ee8d4
Mohamed abdelhady Ibrahim <117633961+EYADHADY@users.noreply.github.com>
2023-07-29 10:45:35 +0300

    Update places_amenities.py

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,68 +1,88 @@
 #!/usr/bin/python3
 """
-Flask route that returns json status response
+View for Reviews that handles all RESTful API actions
 """
+
+from flask import jsonify, request, abort
+from models import storage
+from models.review import Review
 from api.v1.views import app_views
-from flask import abort, jsonify, request
-from models import storage, CNC
-from os import environ
-STORAGE_TYPE = environ.get('HBNB_TYPE_STORAGE')
 
 
-@app_views.route('/places/<place_id>/amenities', methods=['GET'])
-def amenities_per_place(place_id=None):
-    """
-        reviews route to handle http method for requested reviews by place
-    """
-    place_obj = storage.get('Place', place_id)
-
-    if request.method == 'GET':
-        if place_obj is None:
-            abort(404, 'Not found')
-        all_amenities = storage.all('Amenity')
-        if STORAGE_TYPE == 'db':
-            place_amenities = place_obj.amenities
-        else:
-            place_amen_ids = place_obj.amenities
-            place_amenities = []
-            for amen in place_amen_ids:
-                response.append(storage.get('Amenity', amen))
-        place_amenities = [
-            obj.to_json() for obj in place_amenities
-            ]
-        return jsonify(place_amenities)
+@app_views.route('/places/<place_id>/reviews', methods=['GET'],
+                 strict_slashes=False)
+def reviews_all(place_id):
+    """ returns list of all Review objects """
+    place = storage.get("Place", place_id)
+    if place is None:
+        abort(404)
+    reviews_all = []
+    reviews = storage.all("Review").values()
+    for review in reviews:
+        if review.place_id == place_id:
+            reviews_all.append(review.to_json())
+    return jsonify(reviews_all)
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE', 'POST'])
-def amenity_to_place(place_id=None, amenity_id=None):
-    """
-        reviews route to handle http methods for given review by ID
-    """
-    place_obj = storage.get('Place', place_id)
-    amenity_obj = storage.get('Amenity', amenity_id)
-    if place_obj is None:
-        abort(404, 'Not found')
-    if amenity_obj is None:
-        abort(404, 'Not found')
+@app_views.route('/reviews/<review_id>', methods=['GET'])
+def review_get(review_id):
+    """ handles GET method """
+    review = storage.get("Review", review_id)
+    if review is None:
+        abort(404)
+    review = review.to_json()
+    return jsonify(review)
 
-    if request.method == 'DELETE':
-        if (amenity_obj not in place_obj.amenities and
-                amenity_obj.id not in place_obj.amenities):
-            abort(404, 'Not found')
-        if STORAGE_TYPE == 'db':
-            place_obj.amenities.remove(amenity_obj)
-        else:
-            place_obj.amenity_ids.pop(amenity_obj.id, None)
-        place_obj.save()
-        return jsonify({}), 200
 
-    if request.method == 'POST':
-        if (amenity_obj in place_obj.amenities or
-                amenity_obj.id in place_obj.amenities):
-            return jsonify(amenity_obj.to_json()), 200
-        if STORAGE_TYPE == 'db':
-            place_obj.amenities.append(amenity_obj)
-        else:
-            place_obj.amenities = amenity_obj
-        return jsonify(amenity_obj.to_json()), 201
+@app_views.route('/reviews/<review_id>', methods=['DELETE'])
+def review_delete(review_id):
+    """ handles DELETE method """
+    empty_dict = {}
+    review = storage.get("Review", review_id)
+    if review is None:
+        abort(404)
+    storage.delete(review)
+    storage.save()
+    return jsonify(empty_dict), 200
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['POST'],
+                 strict_slashes=False)
+def review_post(place_id):
+    """ handles POST method """
+    place = storage.get("Place", place_id)
+    if place is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    if 'user_id' not in data:
+        abort(400, "Missing user_id")
+    user = storage.get("User", data['user_id'])
+    if user is None:
+        abort(404)
+    if 'text' not in data:
+        abort(400, "Missing text")
+    review = Review(**data)
+    review.place_id = place_id
+    review.save()
+    review = review.to_json()
+    return jsonify(review), 201
+
+
+@app_views.route('/reviews/<review_id>', methods=['PUT'])
+def review_put(review_id):
+    """ handles PUT method """
+    review = storage.get("Review", review_id)
+    if review is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    for key, value in data.items():
+        ignore_keys = ["id", "user_id", "place_id", "created_at", "updated_at"]
+        if key not in ignore_keys:
+            review.bm_update(key, value)
+    review.save()
+    review = review.to_json()
+    return jsonify(review), 200
